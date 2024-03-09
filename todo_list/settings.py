7f3d66eebd117180a6bb5ecc9c8eb944de09baeb
Akshay Parihar <pariharakshay40@gmail.com>
2024-03-09 19:48:07 +0530

    code commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u224j5e1!uwkpxkeh@&=*#284!zly=g9km3kwr_rxn%@gq0*y='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
