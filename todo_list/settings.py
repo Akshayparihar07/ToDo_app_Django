93372d05d3386fa99fb460c3667f1dc9a8216ecc
Akshay Parihar <pariharakshay40@gmail.com>
2024-03-09 19:46:08 +0530

    added vercel.json

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u224j5e1!uwkpxkeh@&=*#284!zly=g9km3kwr_rxn%@gq0*y='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
